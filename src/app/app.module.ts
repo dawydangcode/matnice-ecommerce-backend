6eff1fd7c3a00f2480c7fa23d8a716aa7fcb7ab7
dawydangcode <giahuy260123@gmail.com>
2025-07-18 11:09:02 +0700

    feat: update package.json with new dependencies and scripts
    - Added new dependencies for mailer, JWT, passport, and TypeORM.
    - Included dev dependencies for type definitions and webpack.
    - Added prestart:dev script to install dependencies before starting the development server.
    
    refactor: enhance AppController with Swagger documentation
    
    - Added ApiTags to AppController for better API documentation.
    - Changed the base route to 'api/v1'.
    
    chore: configure TypeORM and environment variables
    
    - Integrated ConfigModule and TypeOrmModule into AppModule.
    - Loaded database configuration from a new config file.
    
    feat: create database configuration file
    
    - Added database.ts to manage database connection settings using environment variables.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import app from 'src/config/app';
+import database from 'src/config/database';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [database, app],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<any>('database') as TypeOrmModuleAsyncOptions;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
