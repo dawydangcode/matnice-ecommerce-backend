cf17737dd9e1630b6c7775f67b2419b77cc9570b
dawydangcode <giahuy260123@gmail.com>
2025-08-13 16:00:41 +0700

    feat(product): add ProductFilterDto for enhanced product filtering capabilities

---
+++ src/product/dtos/product-filter.dto.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  FrameBridgeDesignType,
+  FrameMaterialType,
+  FrameShapeType,
+  FrameStyleType,
+  FrameType,
+} from '../modules/product-detail/enum/frame.type';
+import { ProductType } from '../enum/product.type';
+
+export class ProductFilterDto {
+  @ApiProperty()
+  brandIds?: number[];
+
+  @ApiProperty()
+  categoryIds?: number[];
+
+  @ApiProperty()
+  productType?: ProductType[];
+
+  @ApiProperty()
+  frameShapeTypes?: FrameShapeType[];
+
+  @ApiProperty()
+  frameTypes?: FrameType[];
+
+  @ApiProperty()
+  bridgeDesignTypes?: FrameBridgeDesignType[];
+
+  @ApiProperty()
+  materialTypes?: FrameMaterialType[];
+
+  @ApiProperty()
+  styleTypes?: FrameStyleType[];
+
+  @ApiProperty()
+  priceMin?: number;
+
+  @ApiProperty()
+  priceMax?: number;
+}
