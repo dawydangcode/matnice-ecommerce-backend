4492b467ac231ed6362a8f42b2d069972df1c617
dawydangcode <giahuy260123@gmail.com>
2025-10-14 14:23:23 +0700

    fix(cors): update CORS configuration to include additional methods and headers

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,21 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3002',
+      'https://localhost:3002',
       'https://matnice-ecommerce-frontend.vercel.app',
-      'http://13.54.93.54',
-      'https://13.54.93.54',
       'http://matnice.id.vn',
       'https://matnice.id.vn',
+      'http://13.54.93.54',
+      'https://13.54.93.54',
     ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Accept',
+    ],
     optionsSuccessStatus: 200, // Some legacy browsers choke on 204
   });
 
